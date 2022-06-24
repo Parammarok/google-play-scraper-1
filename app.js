df5db19440117ca54c7adbc68fc5118f8190665d
ParaMMarok <rockingboy3600@gmail.com>
2022-06-24 20:12:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-// index.js
-const express = require('express');|
+const express = require('express');
 const app = express();
-app.listen(3333, () => {
-  console.log('server started!');
+const port = process.env.PORT || 8080;
+var gplay = require('google-play-scraper');
+
+
+app.get('/', function(req, res) {
+    var id = req.query.id;
+         if (id != '') {
+        console.log('Getting Details: ' + id );
+                
+            console.log(gplay.app({appId: id});
+            res.send(gplay.app({appId: id}); 
 });
+
+
+app.listen(port, function() {
+    console.log('App listening on port ' + port)
+})
