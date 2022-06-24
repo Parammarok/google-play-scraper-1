c6b2bfd20a1e9b4cb3a130d7b49cfafeee1cb4ed
ParaMMarok <rockingboy3600@gmail.com>
2022-06-24 19:43:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var gplay = require('google-play-scraper');
+var id = req.query.id;
 
-gplay.app({appId: 'com.google.android.apps.translate'})
+gplay.app({appId: id})
   .then(console.log, console.log);
+res.send(gplay.app({appId: id})); 
