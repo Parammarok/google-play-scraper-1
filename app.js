a88b08f9170a04975a1b491f247f01f4e41caf58
ParaMMarok <rockingboy3600@gmail.com>
2022-06-24 19:58:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-const express = require('express');
+// index.js
+const express = require('express');|
 const app = express();
-var gplay = require('google-play-scraper');
-
-
-app.get('/', function(req, res) {
-var id = req.query.id;
-app.listen(443, () => {
-gplay.app({appId: id})
-  .then(console.log, console.log);
-res.send(id); 
+app.listen(3333, () => {
+  console.log('server started!');
 });
